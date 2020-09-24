f54abeadbc8f07692e7a12115f9c054db2a04fec
the-farshad <farshad.1991@gmail.com>
2020-09-24 17:53:59 +0300

    tiny change

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hello World!'));
+  }
+}
